b55b888a52dbdd89b846b5bb9c86d547475a7dda
Mário Gaudencio <bento@meshapp.net>
2018-06-14 14:34:29 +0100

    change image height for all sliders

--- src/insideNavSlider.jsx
+++ src/insideNavSlider.jsx
@@ -926,7 +926,7 @@ export default class InsideNavSlider extends React.Component {
   _renderItem = (item) => {
     const onImageError = this.props.onImageError || this._handleImageError;
 
-    let clickStyle = this.props.onSliderClick ? { cursor: 'pointer', maxHeight: '500px' } : { maxHeight: '500px' }
+    let clickStyle = this.props.onSliderClick ? { cursor: 'pointer' } : {}
 
     return (
       <div className='image-gallery-image' style={clickStyle} onClick={() => this.sliderClicked(item)}>
